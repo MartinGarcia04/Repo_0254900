5643e080eaa75ae4887bb49497dfa78f81f2df8b
MartinGarcia04 <0254900@up.edu.mx>
2024-09-26 11:57:36 -0600

    field: express_app

--- Portfolio/08-BasicExpress/server.js
+++ Portfolio/08-BasicExpress/server.js
@@ -4,20 +4,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get('/', (req, res) => {
+
+app.route('/')
+.get((req, res) => {
   res.sendFile(__dirname+"/index.html")
+})
+.post((req,res)=>{
+  var weight=req.body.weight;
+  var height=req.body.height;
+  res.send("Your MBI is: "+ weight/(height*height)*10000);
+
 });
 
 app.get('/about', (req, res) => {
     res.send('Hello My Friends')
   });
 
-app.post('/', (req,res)=>{
-  var weight=req.body.weight;
-  var height=req.body.height;
-  res.send("Your MBI is: "+ weight/(height*height)*100+"%");
-
-});
 
 
 app.listen(3000, ()=>{
