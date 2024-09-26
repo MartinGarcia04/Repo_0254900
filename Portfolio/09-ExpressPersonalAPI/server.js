6c151e407e5b5df87e0ab88fbb7516f814fe053c
MartinGarcia04 <0254900@up.edu.mx>
2024-09-26 12:26:19 -0600

    field: UPDATE 09

---
+++ Portfolio/09-ExpressPersonalAPI/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine","ejs");
+
+app.route("/")
+.get((req,res)=>{
+    res.render("index",{})
+});
+
+app.listen(5000,()=>{
+    console.log("Runing in 5000");
+});
