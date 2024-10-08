380ce7b4933337cc912494235d4e71a07b114bbf
MartinGarcia04 <0254900@up.edu.mx>
2024-10-08 12:38:44 -0600

    field: UPDATE 11-API

---
+++ Portfolio/11-Express3rd-partyAPI/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+const apiKey = process.env.API_KEY;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+
+app.listen(7000,()=>{
+    console.log("Listen on port 3000");
+});
